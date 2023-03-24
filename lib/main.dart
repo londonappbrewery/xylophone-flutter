49d0ea72387f67fcc0005cb77aa56130366d4534
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-03-24 22:03:41 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/src/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,9 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(onPressed: () {}, child: Text('Click Me')),
+          ),
         ),
       ),
     );
