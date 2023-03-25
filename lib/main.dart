e4f6ddc34d4a545be9bf9393fb0bbb4e7a8ea90e
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-03-25 23:42:04 +0530

    Audio added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/src/audio_cache.dart';
+import 'package:audiofileplayer/audiofileplayer.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,8 +9,58 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(onPressed: () {}, child: Text('Click Me')),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Audio audio = Audio.load('assets/note1.wav');
+                  audio.play();
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Audio audio = Audio.load('assets/note2.wav');
+                  audio.play();
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Audio audio = Audio.load('assets/note3.wav');
+                  audio.play();
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Audio audio = Audio.load('assets/note4.wav');
+                  audio.play();
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Audio audio = Audio.load('assets/note5.wav');
+                  audio.play();
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Audio audio = Audio.load('assets/note6.wav');
+                  audio.play();
+                },
+                child: Text('Click Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Audio audio = Audio.load('assets/note7.wav');
+                  audio.play();
+                },
+                child: Text('Click Me'),
+              ),
+            ],
           ),
         ),
       ),
